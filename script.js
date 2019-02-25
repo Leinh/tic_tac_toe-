7bd12be1fa3923c462c860cefab30595c4197a08
Leinh <leinhou@gmail.com>
2019-02-25 13:59:34 +0100

    améliorations graphiques du jeu

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ var coup = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // On va définir l'ordre des tours et l'affichage du signe selon le tour. Le nombre de tour va augmenter à chaque clic.
 function image(x) {
-// Ce if sert à définir que si une case a été cliquée, elle ne puisse plus l'être à nouveau.
-// En effet, il est dit qu'il est possible de lancer la fonction affichant le signe correspondant au joueur que si la valeur
-// De la case dans le tableau est égale à 0. Or, dès que l'on clique, on donne une valeur selon le tour durant lequel on a 
-// cliqué.
+    // Ce if sert à définir que si une case a été cliquée, elle ne puisse plus l'être à nouveau.
+    // En effet, il est dit qu'il est possible de lancer la fonction affichant le signe correspondant au joueur que si la valeur
+    // De la case dans le tableau est égale à 0. Or, dès que l'on clique, on donne une valeur selon le tour durant lequel on a 
+    // cliqué.
     if (coup[x - 1] == 0) {
         nb_tour++;
         if (tour == 0) {
@@ -30,14 +30,16 @@ function image(x) {
 
 // On va définir les fonctions qui vont afficher les cercles et les croix:
 function circle(x) {
-    document.getElementById('case'+x).style = "background-image:url(circle.png); background-size:100%;background-repeat:no-repeat";
-    document.getElementById('case'+x).name = "circle";
+    document.getElementById('case' + x).style = "background-image:url(circle.png); background-size:100%;background-repeat:no-repeat";
+    document.getElementById('case' + x).name = "circle";
     document.getElementById("message").innerHTML = "Joueur 2, c'est à vous:";
+    document.getElementById("message").style.color="rgb(81, 123, 212)";
 }
 function cross(x) {
-    document.getElementById('case'+x).style = "background-image:url(cross.png); background-size:100%;background-repeat:no-repeat";
-    document.getElementById('case'+x).name = "cross";
+    document.getElementById('case' + x).style = "background-image:url(cross.png); background-size:100%;background-repeat:no-repeat";
+    document.getElementById('case' + x).name = "cross";
     document.getElementById("message").innerHTML = "Joueur 1, c'est à vous:";
+    document.getElementById("message").style.color="rgb(202, 69, 69)";
 }
 
 
@@ -59,9 +61,6 @@ function play(casex) {
     }
     else {
         coup[casex - 1] = 2;
-       
-
-
     }
     test_victoire();
 
@@ -69,8 +68,8 @@ function play(casex) {
 
 
 function test_victoire() {
-// Ici, on va définir les conditions de victoires par rapport aux valeurs du tableau équivalent à la case.
-// Si trois cases alignées ont une valeur similaire, alors le joueur qui est parvenu à les aligner gagne.
+    // Ici, on va définir les conditions de victoires par rapport aux valeurs du tableau équivalent à la case.
+    // Si trois cases alignées ont une valeur similaire, alors le joueur qui est parvenu à les aligner gagne.
     if (
         // Horizontal:
         ((coup[0] == coup[1] && coup[1] == coup[2]) && coup[0] > 0) ||
@@ -87,6 +86,7 @@ function test_victoire() {
 
         if (tour == 0) {
             document.getElementById("message").innerHTML = "Joueur 2, vous êtes le champion!";
+
         }
         else if (tour == 1) {
             document.getElementById("message").innerHTML = "Joueur 1, vous êtes le champion!";
@@ -95,12 +95,20 @@ function test_victoire() {
 
     }
     else {
-    // Enfin, le cas de l'égalité. Si au tour 9 rien n'est aligné, alors nous avons affaire à une égalité.
+        // Enfin, le cas de l'égalité. Si au tour 9 rien n'est aligné, alors nous avons affaire à une égalité.
         if (nb_tour == 9) {
             document.getElementById("message").innerHTML = "Egalité";
+
         }
 
     }
 
 
+}
+
+
+// Enfin, on va créer la fonction qui permet de reload la page pour recommencer une partie en cliquant sur recommencer:
+
+function reload() {
+    location.reload();
 }
